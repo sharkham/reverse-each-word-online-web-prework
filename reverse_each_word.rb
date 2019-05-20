bfe3d495f272b55709d6eab7e660b688f6b5ebe7
Sam Markham <sam.e.markham@gmail.com>
2019-05-20 20:02:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 def reverse_each_word(sentence)
   array2 = []
   array = sentence.split(" ")
-  array.each { |word| word.reverse }
+  array.each do 
+    |word| word.reverse 
+    array2 << word
+  end
+  array2
 end 
