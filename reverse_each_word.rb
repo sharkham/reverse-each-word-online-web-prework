f5d34348db67ca620ea321973e058136536da937
Sam Markham <sam.e.markham@gmail.com>
2019-05-20 20:05:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
+#def reverse_each_word(sentence)
+  #array2 = []
+  #array = sentence.split(" ")
+  #array.each do 
+    #|word| word.reverse! 
+    #array2 << word
+  #end
+  #array3 = array2.join(" ")
+#end 
+
 def reverse_each_word(sentence)
-  array2 = []
   array = sentence.split(" ")
-  array.each do 
-    |word| word.reverse! 
-    array2 << word
-  end
-  array3 = array2.join(" ")
+  array.collect do 
+    |word| word.reverse!
+  end 
+  string = array.join(" ")
 end 
